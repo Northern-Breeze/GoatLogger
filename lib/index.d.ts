0a85faba391c73d2821d009b67cb192cdcee6973
your username <samuelmthwa79@gmail.com>
2021-01-28 09:44:13 +0200

    update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,15 +1,5 @@
-export declare class GoatLogger {
-    private readonly getTimeStamp;
-    private readonly goatServer;
-    logOnly: boolean;
-    server: string;
-    token: string;
-    constructor(logOnly: boolean, server: string, token: string);
-    info(object: any, namespace: string, message: string): void;
-    error(object: any, namespace: string, message: string): void;
-}
 export declare class BasicLogger {
     private static getTimeStamp;
-    static info(object: any, namespace: string, message: string): void;
-    static error(object: any, namespace: string, message: string): void;
+    static log(method: string, statusCode: any | number, ip: string | undefined, url: string, object: any): void;
+    static info(message: string, object: any): void;
 }
